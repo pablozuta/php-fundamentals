bc9edf72170cf37b707a8c4a339b4ba5cfd51761
pablozuta <pablo.zuta.h@gmail.com>
2023-04-05 14:56:04 -0400

    inicializacion de repositorio

---
+++ primitives.php
@@ -0,0 +1,22 @@
+<?php
+
+// integers
+$myInt = 42;
+echo($myInt);
+
+// floats
+$myFloat = 3.151;
+
+// booleans
+$myBool = true;
+
+// string
+$myString = "This is a String";
+
+// null
+$myNull = null;
+
+// resource
+$myResource = fopen("file.txt", "r");
+
+?>
