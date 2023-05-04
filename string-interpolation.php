c5aea0b95a2fb910b096d89874f58982859fe678
pablozuta <pablo.zuta.h@gmail.com>
2023-05-04 16:08:18 -0400

    se agrega modulo 'string interpolation'

---
+++ string-interpolation.php
@@ -0,0 +1,14 @@
+<?php
+
+$nombre = "Pablo";
+$edad = 41;
+$verdad = true;
+$PI = 3.15362;
+
+echo "Mi nombre es {$nombre}\n";
+echo "Mi edad es {$edad}\n";
+echo "Esto es {$verdad}\n";
+echo "El valor de PI es {$PI}\n";
+
+
+?>
